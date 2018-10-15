180a95d52553266ffb1e5ab7e842e892ffb678bf
Nawar Noori <nawarnoori@tengroup.com>
2018-10-15 09:44:33 +0100

    Added flake8 to project, cleaned up various PEP8 issues, added pip-tools so the dependencies can be kept up-to-date.

--- scan.py
+++ scan.py
@@ -1,61 +1,79 @@
 # Upside Travel, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import boto3
-import clamav
 import copy
 import json
-import metrics
+import os
 import urllib
-from common import *
 from datetime import datetime
 from distutils.util import strtobool
 
-ENV = os.getenv("ENV", "")
+import boto3
+import clamav
+import common
+import metrics
+
+ENV = os.getenv('ENV', '')
 
 
 def event_object(event):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
     if (not bucket) or (not key):
-        print("Unable to retrieve object from event.\n%s" % event)
-        raise Exception("Unable to retrieve object from event.")
-    return s3.Object(bucket, key)
+        print('Unable to retrieve object from event.\n%s' % event)
+        raise Exception('Unable to retrieve object from event.')
+    return common.s3.Object(bucket, key)
+
 
 def verify_s3_object_version(s3_object):
     # validate that we only process the original version of a file, if asked to do so
     # security check to disallow processing of a new (possibly infected) object version
     # while a clean initial version is getting processed
-    # downstream services may consume latest version by mistake and get the infected version instead
-    if str_to_bool(AV_PROCESS_ORIGINAL_VERSION_ONLY):
-        bucketVersioning = s3.BucketVersioning(s3_object.bucket_name)
-        if (bucketVersioning.status == "Enabled"):
-            bucket = s3.Bucket(s3_object.bucket_name)
+    # downstream services may consume latest version by mistake and
+    # get the infected version instead
+    if str_to_bool(common.AV_PROCESS_ORIGINAL_VERSION_ONLY):
+        bucketVersioning = common.s3.BucketVersioning(s3_object.bucket_name)
+        if (bucketVersioning.status == 'Enabled'):
+            bucket = common.s3.Bucket(s3_object.bucket_name)
             versions = list(bucket.object_versions.filter(Prefix=s3_object.key))
             if len(versions) > 1:
-                print("Detected multiple object versions in %s.%s, aborting processing" % (s3_object.bucket_name, s3_object.key))
-                raise Exception("Detected multiple object versions in %s.%s, aborting processing" % (s3_object.bucket_name, s3_object.key))
+                print('Detected multiple object versions in %s.%s, aborting processing' % (
+                    s3_object.bucket_name, s3_object.key,
+                ))
+                raise Exception(
+                    'Detected multiple object versions in %s.%s, aborting processing' % (
+                        s3_object.bucket_name, s3_object.key,
+                    )
+                )
             else:
-                print("Detected only 1 object version in %s.%s, proceeding with processing" % (s3_object.bucket_name, s3_object.key))
+                print('Detected only 1 object version in %s.%s, proceeding with processing' % (
+                    s3_object.bucket_name, s3_object.key,
+                ))
         else:
             # misconfigured bucket, left with no or suspended versioning
-            print("Unable to implement check for original version, as versioning is not enabled in bucket %s" % s3_object.bucket_name)
-            raise Exception("Object versioning is not enabled in bucket %s" % s3_object.bucket_name)
+            print(
+                'Unable to implement check for original version, '
+                'as versioning is not enabled in bucket %s' % s3_object.bucket_name
+            )
+            raise Exception(
+                'Object versioning is not enabled in bucket %s' % s3_object.bucket_name,
+            )
+
 
 def download_s3_object(s3_object, local_prefix):
-    local_path = "%s/%s/%s" % (local_prefix, s3_object.bucket_name, s3_object.key)
-    create_dir(os.path.dirname(local_path))
+    local_path = '%s/%s/%s' % (local_prefix, s3_object.bucket_name, s3_object.key)
+    common.create_dir(os.path.dirname(local_path))
     s3_object.download_file(local_path)
     return local_path
 
@@ -63,82 +81,92 @@ def download_s3_object(s3_object, local_prefix):
 def set_av_metadata(s3_object, result):
     content_type = s3_object.content_type
     metadata = s3_object.metadata
-    metadata[AV_STATUS_METADATA] = result
-    metadata[AV_TIMESTAMP_METADATA] = datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S UTC")
+    metadata[common.AV_STATUS_METADATA] = result
+    metadata[common.AV_TIMESTAMP_METADATA] = datetime.utcnow().strftime('%Y/%m/%d %H:%M:%S UTC')
     s3_object.copy(
         {
             'Bucket': s3_object.bucket_name,
             'Key': s3_object.key
         },
         ExtraArgs={
-            "ContentType": content_type,
-            "Metadata": metadata,
-            "MetadataDirective": "REPLACE"
+            'ContentType': content_type,
+            'Metadata': metadata,
+            'MetadataDirective': 'REPLACE'
         }
     )
 
 
 def set_av_tags(s3_object, result):
-    curr_tags = s3_client.get_object_tagging(Bucket=s3_object.bucket_name, Key=s3_object.key)["TagSet"]
-    new_tags = copy.copy(curr_tags)
-    for tag in curr_tags:
-        if tag["Key"] in [AV_STATUS_METADATA, AV_TIMESTAMP_METADATA]:
-            new_tags.remove(tag)
-    new_tags.append({"Key": AV_STATUS_METADATA, "Value": result})
-    new_tags.append({"Key": AV_TIMESTAMP_METADATA, "Value": datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S UTC")})
-    s3_client.put_object_tagging(
+    curr_tags = common.s3_client.get_object_tagging(
         Bucket=s3_object.bucket_name,
         Key=s3_object.key,
-        Tagging={"TagSet": new_tags}
+    )['TagSet']
+    new_tags = copy.copy(curr_tags)
+    for tag in curr_tags:
+        if tag['Key'] in [common.AV_STATUS_METADATA, common.AV_TIMESTAMP_METADATA]:
+            new_tags.remove(tag)
+    new_tags.append({'Key': common.AV_STATUS_METADATA, 'Value': result})
+    new_tags.append({
+        'Key': common.AV_TIMESTAMP_METADATA,
+        'Value': datetime.utcnow().strftime('%Y/%m/%d %H:%M:%S UTC'),
+    })
+    common.s3_client.put_object_tagging(
+        Bucket=s3_object.bucket_name,
+        Key=s3_object.key,
+        Tagging={'TagSet': new_tags}
     )
+
 
 def sns_start_scan(s3_object):
-    if AV_SCAN_START_SNS_ARN is None:
+    if common.AV_SCAN_START_SNS_ARN is None:
         return
     message = {
-        "bucket": s3_object.bucket_name,
-        "key": s3_object.key,
-        "version": s3_object.version_id,
-        AV_SCAN_START_METADATA: True,
-        AV_TIMESTAMP_METADATA: datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S UTC")
+        'bucket': s3_object.bucket_name,
+        'key': s3_object.key,
+        'version': s3_object.version_id,
+        common.AV_SCAN_START_METADATA: True,
+        common.AV_TIMESTAMP_METADATA: datetime.utcnow().strftime('%Y/%m/%d %H:%M:%S UTC')
     }
-    sns_client = boto3.client("sns")
+    sns_client = boto3.client('sns')
     sns_client.publish(
-        TargetArn=AV_SCAN_START_SNS_ARN,
+        TargetArn=common.AV_SCAN_START_SNS_ARN,
         Message=json.dumps({'default': json.dumps(message)}),
-        MessageStructure="json"
+        MessageStructure='json'
     )
 
+
 def sns_scan_results(s3_object, result):
-    if AV_STATUS_SNS_ARN is None:
+    if common.AV_STATUS_SNS_ARN is None:
         return
     message = {
-        "bucket": s3_object.bucket_name,
-        "key": s3_object.key,
-        "version": s3_object.version_id,
-        AV_STATUS_METADATA: result,
-        AV_TIMESTAMP_METADATA: datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S UTC")
+        'bucket': s3_object.bucket_name,
+        'key': s3_object.key,
+        'version': s3_object.version_id,
+        common.AV_STATUS_METADATA: result,
+        common.AV_TIMESTAMP_METADATA: datetime.utcnow().strftime('%Y/%m/%d %H:%M:%S UTC')
     }
-    sns_client = boto3.client("sns")
+    sns_client = boto3.client('sns')
     sns_client.publish(
-        TargetArn=AV_STATUS_SNS_ARN,
+        TargetArn=common.AV_STATUS_SNS_ARN,
         Message=json.dumps({'default': json.dumps(message)}),
-        MessageStructure="json"
+        MessageStructure='json'
     )
 
 
 def lambda_handler(event, context):
     start_time = datetime.utcnow()
-    print("Script starting at %s\n" %
-          (start_time.strftime("%Y/%m/%d %H:%M:%S UTC")))
+    print('Script starting at %s\n' %
+          (start_time.strftime('%Y/%m/%d %H:%M:%S UTC')))
     s3_object = event_object(event)
     verify_s3_object_version(s3_object)
     sns_start_scan(s3_object)
-    file_path = download_s3_object(s3_object, "/tmp")
-    clamav.update_defs_from_s3(AV_DEFINITION_S3_BUCKET, AV_DEFINITION_S3_PREFIX)
+    file_path = download_s3_object(s3_object, '/tmp')
+    clamav.update_defs_from_s3(common.AV_DEFINITION_S3_BUCKET, common.AV_DEFINITION_S3_PREFIX)
     scan_result = clamav.scan_file(file_path)
-    print("Scan of s3://%s resulted in %s\n" % (os.path.join(s3_object.bucket_name, s3_object.key), scan_result))
-    if "AV_UPDATE_METADATA" in os.environ:
+    print('Scan of s3://%s resulted in %s\n' % (
+        os.path.join(s3_object.bucket_name, s3_object.key), scan_result,
+    ))
+    if 'AV_UPDATE_METADATA' in os.environ:
         set_av_metadata(s3_object, scan_result)
     set_av_tags(s3_object, scan_result)
     sns_scan_results(s3_object, scan_result)
@@ -148,8 +176,9 @@ def lambda_handler(event, context):
         os.remove(file_path)
     except OSError:
         pass
-    print("Script finished at %s\n" %
-          datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S UTC"))
+    print('Script finished at %s\n' %
+          datetime.utcnow().strftime('%Y/%m/%d %H:%M:%S UTC'))
+
 
 def str_to_bool(s):
     return bool(strtobool(str(s)))
--- update.py
+++ update.py
@@ -1,37 +1,42 @@
 # Upside Travel, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import clamav
-from common import *
-from datetime import datetime
 import os
+from datetime import datetime
+
+import clamav
+import common
 
 
 def lambda_handler(event, context):
     start_time = datetime.utcnow()
-    print("Script starting at %s\n" %
-          (start_time.strftime("%Y/%m/%d %H:%M:%S UTC")))
-    clamav.update_defs_from_s3(AV_DEFINITION_S3_BUCKET, AV_DEFINITION_S3_PREFIX)
-    clamav.update_defs_from_freshclam(AV_DEFINITION_PATH, CLAMAVLIB_PATH)
+    print('Script starting at %s\n' %
+          (start_time.strftime('%Y/%m/%d %H:%M:%S UTC')))
+    clamav.update_defs_from_s3(common.AV_DEFINITION_S3_BUCKET, common.AV_DEFINITION_S3_PREFIX)
+    clamav.update_defs_from_freshclam(common.AV_DEFINITION_PATH, common.CLAMAVLIB_PATH)
     # If main.cvd gets updated (very rare), we will need to force freshclam
     # to download the compressed version to keep file sizes down.
     # The existence of main.cud is the trigger to know this has happened.
-    if os.path.exists(os.path.join(AV_DEFINITION_PATH, "main.cud")):
-        os.remove(os.path.join(AV_DEFINITION_PATH, "main.cud"))
-        if os.path.exists(os.path.join(AV_DEFINITION_PATH, "main.cvd")):
-            os.remove(os.path.join(AV_DEFINITION_PATH, "main.cvd"))
-        clamav.update_defs_from_freshclam(AV_DEFINITION_PATH, CLAMAVLIB_PATH)
-    clamav.upload_defs_to_s3(AV_DEFINITION_S3_BUCKET, AV_DEFINITION_S3_PREFIX, AV_DEFINITION_PATH)
-    print("Script finished at %s\n" %
-          datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S UTC"))
+    if os.path.exists(os.path.join(common.AV_DEFINITION_PATH, 'main.cud')):
+        os.remove(os.path.join(common.AV_DEFINITION_PATH, 'main.cud'))
+        if os.path.exists(os.path.join(common.AV_DEFINITION_PATH, 'main.cvd')):
+            os.remove(os.path.join(common.AV_DEFINITION_PATH, 'main.cvd'))
+        clamav.update_defs_from_freshclam(common.AV_DEFINITION_PATH, common.CLAMAVLIB_PATH)
+    clamav.upload_defs_to_s3(
+        common.AV_DEFINITION_S3_BUCKET,
+        common.AV_DEFINITION_S3_PREFIX,
+        common.AV_DEFINITION_PATH,
+    )
+    print('Script finished at %s\n' %
+          datetime.utcnow().strftime('%Y/%m/%d %H:%M:%S UTC'))
